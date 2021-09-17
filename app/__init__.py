db325732a9b458df88bedef92253119f663d1601
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-09-18 01:56:25 +0300

    login section done

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
-# from flask_uploads import UploadSet, configure_uploads, IMAGES
-# from flask_mail import Mail
-# from flask_simplemde import SimpleMDE
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 # Initializing application
 
@@ -20,9 +26,8 @@ def create_app(config_name):
 
     # Initializing flask extensions
     db.init_app(app)
-    # login_manager = LoginManager(app)
-    # login_manager.session_protection = 'strong'
-    # login_manager.login_view = 'auth.login'
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
     # mail = Mail(app)
     # simple = SimpleMDE(app)
 
@@ -31,8 +36,8 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     # Registering the auth bluprints
-    # from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/user-account')
 
     # setting config
     # from .requests import configure_request
---
+++ app/auth/views.py
@@ -0,0 +1,47 @@
+from flask import render_template, redirect, url_for, flash, request
+from . import auth
+from flask_login import login_user, logout_user, login_required
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+from .. import db
+# from ..email import mail_message
+
+
+# register new user
+@auth.route('/register')
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+    title = "Create New Account"
+    return render_template('auth/register.html', title=title, registration_form=form)
+
+
+# login user
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid email or Password')
+
+    title = "Login to your account"
+    return render_template('auth/login.html', login_form=login_form, title=title)
+
+
+# log out user
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
