767aa06bff82760da2001005472f434b28b1c114
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-09-19 20:57:35 +0300

    deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY='here you place the secret key'
-export MAIL_USERNAME=wilson.wezaprosoft@gmail.com
-export MAIL_PASSWORD=wil@2020
+export MAIL_USERNAME=
+export MAIL_PASSWORD=
 python3.8 manage.py server
